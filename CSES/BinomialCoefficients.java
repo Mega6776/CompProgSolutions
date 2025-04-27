06d05d65fe3eae9189610d1ed752313f4190e184
Mega6776 <165506547+Mega6776@users.noreply.github.com>
2025-04-27 17:03:09 -0500

    Create BinomialCoefficients.java

---
+++ CSES/BinomialCoefficients.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+public class BinomialCoefficients
+{
+  static long MOD=1000000000+7;
+  static long exp(long a,long b)
+  {
+    long res=1;
+    a%=MOD;
+    while(b>0)
+    {
+      if(b%2==1)
+      res=(res*a)%MOD;
+      a=(a*a)%MOD;
+      b/=2;
+    }
+    return res;
+  }
+    public static void main(String[] args) throws IOException
+    {
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      PrintWriter pw=new PrintWriter(System.out);
+      StringTokenizer st=new StringTokenizer(br.readLine());
+      int t=Integer.parseInt(st.nextToken());
+      long[]fac=new long[1000001];
+      fac[0]=1;
+      for(int i=1;i<fac.length;i++)
+      fac[i]=i*fac[i-1]%MOD;
+      long[]modFac=new long[1000001];
+      modFac[1000000]=exp(fac[1000000],MOD-2);
+      for(int i=modFac.length-1;i>=1;i--)
+      modFac[i-1]=modFac[i]*i%MOD;
+      while(t-->0)
+      {
+        st=new StringTokenizer(br.readLine());
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        pw.println(fac[a]*modFac[b]%MOD*modFac[a-b]%MOD);
+      }
+      pw.close();
+    }
+}
